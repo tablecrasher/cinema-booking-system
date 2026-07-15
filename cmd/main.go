1ed60b73efe3972087856b77ecdef89463b554a4
Benjamin Khong <benkhong27@gmail.com>
2026-07-14 18:17:48 -0700

    built web api

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /movies", listMovies)
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
+
+var movies = []movieResponse{
+	{ID: "obsession", Title: "Obsession", Rows: 5, SeatsPerRow: 8},
+	{ID: "jurassicworld", Title: "Jurassic World", Rows: 4, SeatsPerRow: 6},
+}
+
+func listMovies(w http.ResponseWriter, r *http.Request) {
+	WriteJSON(w, http.StatusOK, movies)
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(v)
+}
+
+type movieResponse struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Rows        int    `json:"rows"`
+	SeatsPerRow int    `json:"seats_per_row"`
 }
